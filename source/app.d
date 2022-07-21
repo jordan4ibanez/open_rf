b6ac5fc6834304442110611e85cc04264643d94d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-21 05:46:38 -0400

    Import and utilize fish

--- source/app.d
+++ source/app.d
@@ -1,21 +1,9 @@
 import std.stdio;
+import fish;
 
-class Test {
-	double life = 5;
-	this(double standardLife) {
-		this.life = standardLife;
-	}
-	this() {}
-	void addLife(double adder) {
-		this.life += adder;
-	}
-	void debugger() {
-		writeln("I am at ", this.life, " health points!");
-	}
-}
 
 void main() {
-	Test person = new Test();
-	person.addLife(55);
-	person.debugger();
+	Fish fishy = new Fish();
+
+	fishy.debugger();
 }
