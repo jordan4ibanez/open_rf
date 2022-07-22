728edc52583f431ad52ef525d44213a0c5517559
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-22 17:53:13 -0400

    Turn Fish class into FishContainer ECS struct

--- source/fish.d
+++ source/fish.d
@@ -4,25 +4,90 @@ import std.stdio;
 import raylib;
 
 
-class Fish {
-	Vector3 position = Vector3(0,0,0);
-	double life = 5;
+struct FishContainer {
+
+	// Basic fields for now
+	bool[]    exists;
+	Vector3[] position;
+	double[]  life;
     
-	this(double standardLife, Vector3 position) {
-		this.life = standardLife;
-		this.position.x = position.x;
-		this.position.y = position.y;
-		this.position.z = position.z;
+	void newFish(Vector3 position, double standardLife) {
+
+		// Check if there are any free slots
+		bool found      = false;
+		ulong indexFound = 0;
+		ulong currentSize = this.exists.length;
+
+		// Simple fix to avoid crash on unitialized array
+		if (currentSize > 0) {
+			for (ulong i = 0; i < this.exists.length; i++) {
+				if (!this.exists[i]) {
+					found      = true;
+					indexFound = i;
+					break;
+				}
+			}
+		}
+
+		// Overwrite if a free slot is found
+		if (found) {
+			this.position[indexFound] = position;
+			this.life[indexFound]     = standardLife;
+			this.exists[indexFound]   = true;
+		}
+		// Else append to the end of dynamic array
+		else {
+			this.position ~= position;
+			this.life     ~= standardLife;
+			this.exists   ~= true;
+		}
+		
 	}
 
-	void addLife(double adder) {
-		this.life += adder;
-	}
-	void debugger() {
-		writeln("I am at ", this.life, " health points!");
+	// Resets the entire ECS to allow further reuse
+	void reset() {
+		this.exists[] = false;
 	}
 
-	void printPos() {
-		writeln(this.position);
+	// Allows systems to remove fish
+	void removeFish(ulong ID) {
+		this.exists[ID] = false;
+	}
+
+	// Allows systems to get all fish that exist, or nothing
+	ulong[] collectIDs() {
+		ulong[] idCollection;
+		ulong length = this.exists.length;
+
+		// Skip CPU cycles and just return unitialized array
+		if (length <= 0) {
+			return idCollection;
+		}
+		
+		// Get all fish that exist
+		for (ulong i = 0; i < length; i++) {
+			if (this.exists[i]) {
+				idCollection ~= i;
+			}
+		}
+
+		return idCollection;
+	}
+
+	// Adds life to a fish, this is a placeholder debug
+	void addLife(ulong id, double adder) {
+		this.life[id] += adder;
+	}
+
+	// This will heavily change in the future
+	void debugger(ulong id) {
+		writeln("FISH ID: ", id,
+		" | I am at X: ", this.position[id].x,
+		" | Y: ", this.position[id].y,
+		" | Z: ", this.position[id].z);
+	}
+
+	void printPos(ulong id) {
+		writeln(this.position[id]);
 	}
 }
