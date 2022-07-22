020870f62d10d51b0ed57886502b64c6c7f7eda9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-21 22:19:15 -0400

    Give fish position

--- source/fish.d
+++ source/fish.d
@@ -1,18 +1,28 @@
 module fish;
 
 import std.stdio;
+import raylib;
+
 
 class Fish {
+	Vector3 position = Vector3(0,0,0);
 	double life = 5;
     
-	this(double standardLife) {
+	this(double standardLife, Vector3 position) {
 		this.life = standardLife;
+		this.position.x = position.x;
+		this.position.y = position.y;
+		this.position.z = position.z;
 	}
-	this() {}
+
 	void addLife(double adder) {
 		this.life += adder;
 	}
 	void debugger() {
 		writeln("I am at ", this.life, " health points!");
+	}
+
+	void printPos() {
+		writeln(this.position);
 	}
 }
