dec84d24e4cec63d8ce2b378a006c8e66d982712
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-23 02:07:13 -0400

    Create a basic fish definition

--- source/fish_definition.d
+++ source/fish_definition.d
@@ -1,2 +1,30 @@
 module fish_definition;
 
+import std.string;
+
+struct FishDefinition {
+    string name;
+    double scale;
+    double minNormalSpeed;
+	double maxNormalSpeed;
+	double minSprintSpeed;
+	double maxSprintSpeed;
+
+    this(
+        string name,
+        double scale,
+        double minNormalSpeed,
+        double maxNormalSpeed,
+        double minSprintSpeed,
+        double maxSprintSpeed) {
+
+        this.name = name;
+        this.scale = scale;
+        this.minNormalSpeed = minNormalSpeed;
+        this.maxNormalSpeed = maxNormalSpeed;
+        this.minSprintSpeed = minSprintSpeed;
+        this.maxSprintSpeed = maxSprintSpeed;
+    }
+
+
+}
