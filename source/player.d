1421c0363b77729c33732b9d6fce84a64c25b5f5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-27 17:21:25 -0400

    Begin prototyping of interface into raylib camera

--- source/player.d
+++ source/player.d
@@ -1,10 +1,9 @@
 module player;
 
+import std.stdio;
 import raylib;
 
 struct Player {
-
-    static double tester = 0;
 
     // Position is the base of the player, centered
     Vector3 position;
@@ -15,5 +14,29 @@ struct Player {
     this(Vector3 startPosition) {
         this.position = startPosition;
         this.inertia = Vector3(0,0,0);
+    }
+
+    Vector3 getPos() {
+        return this.position;
+    }
+
+    Vector3 getCameraPos() {
+        return Vector3(
+            this.position.x,
+            this.position.y + this.eyeHeight,
+            this.position.z
+        );
+    }
+
+    void onTick(double delta, Camera camera) {
+        //writeln(camera.target);
+        writeln(camera.position);
+
+        if (IsKeyDown(KeyboardKey.KEY_W)) {
+            Vector2 direction = Vector2(camera.target.x, camera.target.z);
+
+            this.position.x += direction.x * delta;
+            this.position.z += direction.y * delta;
+        }
     }
 }
